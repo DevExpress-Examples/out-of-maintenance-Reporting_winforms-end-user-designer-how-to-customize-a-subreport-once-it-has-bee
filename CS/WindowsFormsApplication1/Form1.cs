76632d8b5f35e04e7c6e06a4f89165fb912a45e6
Vasily (DevExpress Support) <vasyaste@users.noreply.github.com>
2020-04-10 10:41:03 +0300

    Upgrade to 19.2

--- CS/WindowsFormsApplication1/Form1.cs
+++ CS/WindowsFormsApplication1/Form1.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using DevExpress.DataAccess.ConnectionParameters;
-using DevExpress.DataAccess.Sql;
 using DevExpress.XtraReports.UI;
-using DevExpress.XtraReports.UserDesigner;
-using DevExpress.XtraReports.UserDesigner.Native;
 
 namespace WindowsFormsApplication1 {
     public partial class Form1 : Form {
@@ -21,85 +10,10 @@ namespace WindowsFormsApplication1 {
 
         private void button1_Click(object sender, EventArgs e) {
             ReportDesignTool dt = new ReportDesignTool(new XtraReport1());
-            dt.DesignForm.DesignMdiController.AddCommandHandler(new MyCommandHandler(dt.DesignForm.DesignMdiController));
-            dt.ShowDesignerDialog();
+            dt.DesignRibbonForm.DesignMdiController.AddCommandHandler(new OpenSubreportCommandHandler(dt.DesignRibbonForm.DesignMdiController));
+            dt.ShowRibbonDesignerDialog();
         }
     }
 
-    public class MyCommandHandler : ICommandHandler {
-        XRDesignMdiController mdiController;
-
-        public MyCommandHandler(XRDesignMdiController controller) {
-            mdiController = controller;
-        }
-
-        #region ICommandHandler Members
-
-        public bool CanHandleCommand(ReportCommand command, ref bool useNextHandler) {
-            useNextHandler = command != ReportCommand.OpenSubreport;
-            return !useNextHandler;
-        }
-
-        public void HandleCommand(ReportCommand command, object[] args) {
-            XRDesignPanel panel = mdiController.ActiveDesignPanel;
-            IWindowsService windowsSvc = panel.GetService(typeof(IWindowsService)) as IWindowsService;
-            windowsSvc.EditSubreport((XRSubreport)args[0]);
-            panel = mdiController.ActiveDesignPanel;
-            IDesignerHost host = panel.GetService(typeof(IDesignerHost)) as IDesignerHost;
-            if(host.Container.Components.OfType<SqlDataSource>().FirstOrDefault() == null) {
-
-                Access97ConnectionParameters parameters = new Access97ConnectionParameters(@"|DataDirectory|\nwind.mdb", "", "");
-                SqlDataSource ds = new SqlDataSource(parameters);
-
-                CustomSqlQuery query = new CustomSqlQuery();
-                query.Name = "customQuery1";
-                query.Sql = "SELECT * FROM Products";
-                ds.Queries.Add(query);
-                ds.RebuildResultSchema();
-
-                host.Container.Add(ds);
-
-                panel.Report.DataSource = ds;
-                panel.Report.DataMember = "customQuery1";
-
-
-                int colCount = 3;
-                int colWidth = (panel.Report.PageWidth - (panel.Report.Margins.Left + panel.Report.Margins.Right)) / colCount;
-
-                XRTable tableDetail = new XRTable();
-                tableDetail.Height = 20;
-                tableDetail.Width = (panel.Report.PageWidth - (panel.Report.Margins.Left + panel.Report.Margins.Right));
-                
-                tableDetail.BeginInit();
-
-                XRTableRow detailRow = new XRTableRow();
-                detailRow.Width = tableDetail.Width;
-                tableDetail.Rows.Add(detailRow);
-                host.Container.Add(detailRow);
-
-                XRTableCell detailCell = new XRTableCell();
-                detailCell.Width = colWidth;
-                detailCell.DataBindings.Add("Text", null, "customQuery1.ProductName");
-                detailRow.Cells.Add(detailCell);
-
-                detailCell = new XRTableCell();
-                detailCell.Width = colWidth;
-                detailCell.DataBindings.Add("Text", null, "customQuery1.QuantityPerUnit");
-                detailRow.Cells.Add(detailCell);
-
-                detailCell = new XRTableCell();
-                detailCell.Width = colWidth;
-                detailCell.DataBindings.Add("Text", null, "customQuery1.UnitPrice");
-                detailRow.Cells.Add(detailCell);
-
-                tableDetail.EndInit();
-
-                panel.Report.Bands[BandKind.Detail].Controls.Add(tableDetail);
-
-                host.Container.Add(tableDetail);
-            }
-        }
-
-        #endregion
-    }
+    
 }
---
+++ CS/WindowsFormsApplication1/OpenSubreportCommandHandler.cs
@@ -0,0 +1,96 @@
+﻿using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraReports.UserDesigner;
+using DevExpress.XtraReports.UserDesigner.Native;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+
+namespace WindowsFormsApplication1 {
+    public class OpenSubreportCommandHandler : ICommandHandler {
+        XRDesignMdiController mdiController;
+
+        public OpenSubreportCommandHandler(XRDesignMdiController controller) {
+            mdiController = controller;
+        }
+
+        #region ICommandHandler Members
+
+        bool ICommandHandler.CanHandleCommand(ReportCommand command, ref bool useNextHandler) {
+            useNextHandler = command != ReportCommand.OpenSubreport;
+            return !useNextHandler;
+        }
+
+        void ICommandHandler.HandleCommand(ReportCommand command, object[] args) {
+            XRDesignPanel panel = mdiController.ActiveDesignPanel;
+            IWindowsService windowsSvc = panel.GetService(typeof(IWindowsService)) as IWindowsService;
+            windowsSvc.EditSubreport((XRSubreport)args[0]);
+            panel = mdiController.ActiveDesignPanel;
+            IDesignerHost host = panel.GetService(typeof(IDesignerHost)) as IDesignerHost;
+            if (host.Container.Components.OfType<SqlDataSource>().FirstOrDefault() == null) {
+                CreateReport(panel, host);
+                panel.Report.Site.Name = "MyReport";
+            }
+        }
+        #endregion
+
+        private void CreateReport(XRDesignPanel panel, IDesignerHost host) {
+            Access97ConnectionParameters parameters = new Access97ConnectionParameters(@"|DataDirectory|\nwind.mdb", "", "");
+            SqlDataSource ds = new SqlDataSource(parameters);
+
+            SelectQuery query = SelectQueryFluentBuilder.AddTable("Products").SelectAllColumns().Build("Products");
+            ds.Queries.Add(query);
+            ds.RebuildResultSchema();
+
+            host.Container.Add(ds);
+
+            panel.Report.DataSource = ds;
+            panel.Report.DataMember = query.Name;
+
+
+            int colCount = 3;
+            float colWidth = (panel.Report.PageWidth - (panel.Report.Margins.Left + panel.Report.Margins.Right)) / (float)colCount;
+
+            XRTable tableDetail = new XRTable();
+            tableDetail.HeightF = 25f;
+            tableDetail.WidthF = (panel.Report.PageWidth - (panel.Report.Margins.Left + panel.Report.Margins.Right));
+
+            tableDetail.BeginInit();
+
+            XRTableRow detailRow = new XRTableRow();
+            detailRow.WidthF = tableDetail.WidthF;
+            tableDetail.Rows.Add(detailRow);
+            host.Container.Add(detailRow);
+
+            XRTableCell detailCell = new XRTableCell();
+            detailCell.WidthF = colWidth;
+            detailCell.ExpressionBindings.Add(new ExpressionBinding("BeforePrint", "Text", "[ProductName]"));
+            detailRow.Cells.Add(detailCell);
+            host.Container.Add(detailCell);
+
+            detailCell = new XRTableCell();
+            detailCell.WidthF = colWidth;
+            detailCell.ExpressionBindings.Add(new ExpressionBinding("BeforePrint", "Text", "[QuantityPerUnit]"));
+            detailRow.Cells.Add(detailCell);
+            host.Container.Add(detailCell);
+
+            detailCell = new XRTableCell();
+            detailCell.WidthF = colWidth;
+            detailCell.ExpressionBindings.Add(new ExpressionBinding("BeforePrint", "Text", "[UnitPrice]"));
+            detailRow.Cells.Add(detailCell);
+            host.Container.Add(detailCell);
+
+            tableDetail.EndInit();
+
+            panel.Report.Bands[BandKind.Detail].HeightF = 25f;
+            panel.Report.Bands[BandKind.Detail].Controls.Add(tableDetail);
+
+            host.Container.Add(tableDetail);
+        }
+
+
+
+    }
+}
